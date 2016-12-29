61fecc9725d478810ca1c5307f97053600c96884
Andrus Adamchik <andrus@objectstyle.com>
2016-12-29 22:16:54 +0200

    Bootique 0.21 and annotated config help #17

--- bootique-jersey-client/src/test/java/io/bootique/jersey/client/HttpClientFactoryFactory_ConfigIT.java
+++ bootique-jersey-client/src/test/java/io/bootique/jersey/client/HttpClientFactoryFactory_ConfigIT.java
@@ -8,11 +8,9 @@ import io.bootique.config.PolymorphicConfiguration;
 import io.bootique.config.TypesFactory;
 import io.bootique.config.jackson.JsonNodeConfigurationFactory;
 import io.bootique.jackson.DefaultJacksonService;
-import io.bootique.jackson.ImmutableSubtypeResolver;
 import io.bootique.jackson.JacksonService;
 import io.bootique.jersey.client.auth.AuthenticatorFactory;
 import io.bootique.jersey.client.auth.BasicAuthenticatorFactory;
-import io.bootique.log.BootLogger;
 import io.bootique.log.DefaultBootLogger;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,57 +23,58 @@ import static org.junit.Assert.assertTrue;
 
 public class HttpClientFactoryFactory_ConfigIT {
 
-    private BootLogger bootLogger;
     private TypesFactory<PolymorphicConfiguration> typesFactory;
 
     @Before
     public void before() {
-        bootLogger = new DefaultBootLogger(true);
-        typesFactory = new TypesFactory<>(getClass().getClassLoader(), PolymorphicConfiguration.class, bootLogger);
+        typesFactory = new TypesFactory<>(
+                getClass().getClassLoader(),
+                PolymorphicConfiguration.class,
+                new DefaultBootLogger(true));
     }
 
-	private ConfigurationFactory factory(String yaml) {
+    private ConfigurationFactory factory(String yaml) {
 
-		// not using a mock; making sure all Jackson extensions are loaded
-		JacksonService jacksonService = new DefaultJacksonService(new ImmutableSubtypeResolver(typesFactory.getTypes()), bootLogger);
+        // not using a mock; making sure all Jackson extensions are loaded
+        JacksonService jacksonService = new DefaultJacksonService(typesFactory.getTypes());
 
-		YAMLParser parser;
-		try {
-			parser = new YAMLFactory().createParser(yaml);
-			JsonNode rootNode = jacksonService.newObjectMapper().readTree(parser);
-			return new JsonNodeConfigurationFactory(rootNode, jacksonService.newObjectMapper());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        YAMLParser parser;
+        try {
+            parser = new YAMLFactory().createParser(yaml);
+            JsonNode rootNode = jacksonService.newObjectMapper().readTree(parser);
+            return new JsonNodeConfigurationFactory(rootNode, jacksonService.newObjectMapper());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Test
-	public void testClientFlags() {
-		HttpClientFactoryFactory factory = factory(
-				"r:\n  followRedirects: true\n  connectTimeoutMs: 78\n  readTimeoutMs: 66\n  asyncThreadPoolSize: 44\n")
-						.config(HttpClientFactoryFactory.class, "r");
+    @Test
+    public void testClientFlags() {
+        HttpClientFactoryFactory factory = factory(
+                "r:\n  followRedirects: true\n  connectTimeoutMs: 78\n  readTimeoutMs: 66\n  asyncThreadPoolSize: 44\n")
+                .config(HttpClientFactoryFactory.class, "r");
 
-		assertEquals(true, factory.followRedirects);
-		assertEquals(78, factory.connectTimeoutMs);
-		assertEquals(66, factory.readTimeoutMs);
-		assertEquals(44, factory.asyncThreadPoolSize);
-	}
+        assertEquals(true, factory.followRedirects);
+        assertEquals(78, factory.connectTimeoutMs);
+        assertEquals(66, factory.readTimeoutMs);
+        assertEquals(44, factory.asyncThreadPoolSize);
+    }
 
-	@Test
-	public void testAuthTypes() {
-		HttpClientFactoryFactory factory = factory(
-				"r:\n  auth:\n    a1:\n      type: basic\n      username: u1\n      password: p1\n")
-						.config(HttpClientFactoryFactory.class, "r");
+    @Test
+    public void testAuthTypes() {
+        HttpClientFactoryFactory factory = factory(
+                "r:\n  auth:\n    a1:\n      type: basic\n      username: u1\n      password: p1\n")
+                .config(HttpClientFactoryFactory.class, "r");
 
-		assertNotNull(factory.auth);
-		assertEquals(1, factory.auth.size());
+        assertNotNull(factory.auth);
+        assertEquals(1, factory.auth.size());
 
-		AuthenticatorFactory authFactory = factory.auth.get("a1");
-		assertNotNull(authFactory);
-		assertTrue(authFactory instanceof BasicAuthenticatorFactory);
+        AuthenticatorFactory authFactory = factory.auth.get("a1");
+        assertNotNull(authFactory);
+        assertTrue(authFactory instanceof BasicAuthenticatorFactory);
 
-		BasicAuthenticatorFactory basicAuthFactory = (BasicAuthenticatorFactory) authFactory;
-		assertEquals("u1", basicAuthFactory.getUsername());
-		assertEquals("p1", basicAuthFactory.getPassword());
-	}
+        BasicAuthenticatorFactory basicAuthFactory = (BasicAuthenticatorFactory) authFactory;
+        assertEquals("u1", basicAuthFactory.getUsername());
+        assertEquals("p1", basicAuthFactory.getPassword());
+    }
 }
