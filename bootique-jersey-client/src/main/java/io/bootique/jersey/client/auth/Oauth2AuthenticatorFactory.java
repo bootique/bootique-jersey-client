b0da51185561c6f836bde29cafef26ab6b7b1bf9
Andrus Adamchik <andrus@objectstyle.com>
2017-08-09 17:58:22 +0300

    refactoring .. Oauth2AuthenticatorFactory can be used outside of Bootique and extended, so exposing ivars and functions

--- bootique-jersey-client/src/main/java/io/bootique/jersey/client/auth/Oauth2AuthenticatorFactory.java
+++ bootique-jersey-client/src/main/java/io/bootique/jersey/client/auth/Oauth2AuthenticatorFactory.java
@@ -25,96 +25,97 @@ import java.util.Objects;
  */
 @JsonTypeName("oauth2")
 @BQConfig("Authenticator for Oauth2 protocol. Includes URL of the OAuth token endpoint and " +
-		"username/password that are exchanged for the token.")
+        "username/password that are exchanged for the token.")
 public class Oauth2AuthenticatorFactory implements AuthenticatorFactory {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Oauth2AuthenticatorFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Oauth2AuthenticatorFactory.class);
 
-	protected String tokenUrl;
-	protected String username;
-	protected String password;
+    protected String tokenUrl;
+    protected String username;
+    protected String password;
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	@BQConfigProperty("Login username. A part of the application credentials to obtain oauth token.")
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @BQConfigProperty("Login username. A part of the application credentials to obtain oauth token.")
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	@BQConfigProperty("Password. A part of the application credentials to obtain oauth token.")
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @BQConfigProperty("Password. A part of the application credentials to obtain oauth token.")
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getTokenUrl() {
-		return tokenUrl;
-	}
+    public String getTokenUrl() {
+        return tokenUrl;
+    }
 
-	@BQConfigProperty("A URL of the OAuth2 Token API endpoint.")
-	public void setTokenUrl(String tokenUrl) {
-		this.tokenUrl = tokenUrl;
-	}
+    @BQConfigProperty("A URL of the OAuth2 Token API endpoint.")
+    public void setTokenUrl(String tokenUrl) {
+        this.tokenUrl = tokenUrl;
+    }
 
-	@Override
-	public ClientRequestFilter createAuthFilter(Configuration clientConfig, Injector injector) {
-		return new OAuth2TokenAuthenticator(() -> getToken(clientConfig));
-	}
+    @Override
+    public ClientRequestFilter createAuthFilter(Configuration clientConfig, Injector injector) {
+        return new OAuth2TokenAuthenticator(() -> getToken(clientConfig));
+    }
 
-	protected String getToken(Configuration configuration) {
-		Objects.requireNonNull(username);
-		Objects.requireNonNull(password);
-		Objects.requireNonNull(tokenUrl);
+    protected String getToken(Configuration configuration) {
+        
+        Response tokenResponse = requestToken(configuration);
 
-		LOGGER.info("reading OAuth2 token from " + tokenUrl);
+        try {
+            String token = readToken(tokenResponse);
+            LOGGER.info("Successfully obtained OAuth2 token");
+            return token;
+        } finally {
+            tokenResponse.close();
+        }
+    }
 
-		Response tokenResponse = requestToken(configuration);
+    protected Response requestToken(Configuration configuration) {
 
-		try {
-			String token = readToken(tokenResponse);
-			LOGGER.info("Successfully obtained OAuth2 token");
-			return token;
-		} finally {
-			tokenResponse.close();
-		}
-	}
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+        Objects.requireNonNull(tokenUrl);
 
-	protected Response requestToken(Configuration configuration) {
-		BasicAuthenticator tokenAuth = new BasicAuthenticator(username, password);
+        LOGGER.info("reading OAuth2 token from " + tokenUrl);
+        BasicAuthenticator tokenAuth = new BasicAuthenticator(username, password);
 
-		Entity<String> postEntity = Entity.entity("grant_type=client_credentials",
-				MediaType.APPLICATION_FORM_URLENCODED_TYPE);
-		return ClientBuilder.newClient(configuration).register(tokenAuth)
-				.register(JacksonFeature.class).target(tokenUrl).request().post(postEntity);
-	}
+        Entity<String> postEntity = Entity.entity("grant_type=client_credentials",
+                MediaType.APPLICATION_FORM_URLENCODED_TYPE);
+        return ClientBuilder.newClient(configuration).register(tokenAuth)
+                .register(JacksonFeature.class).target(tokenUrl).request().post(postEntity);
+    }
 
-	protected String readToken(Response response) {
+    protected String readToken(Response response) {
 
-		if (response.getStatus() != Status.OK.getStatusCode()) {
-			String json = response.readEntity(String.class);
-			String message = String.format("Error reading token: %s ... %s", response.getStatus(), json);
-			throw new RuntimeException(message);
-		}
+        if (response.getStatus() != Status.OK.getStatusCode()) {
+            String json = response.readEntity(String.class);
+            String message = String.format("Error reading token: %s ... %s", response.getStatus(), json);
+            throw new RuntimeException(message);
+        }
 
-		Token token = response.readEntity(Token.class);
-		return Objects.requireNonNull(token).getAccessToken();
-	}
+        Token token = response.readEntity(Token.class);
+        return Objects.requireNonNull(token).getAccessToken();
+    }
 
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Token {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Token {
 
-		// TODO: expires, refresh token, etc...
+        // TODO: expires, refresh token, etc...
 
-		@XmlAttribute(name = "access_token")
-		private String accessToken;
+        @XmlAttribute(name = "access_token")
+        private String accessToken;
 
-		public String getAccessToken() {
-			return accessToken;
-		}
-	}
+        public String getAccessToken() {
+            return accessToken;
+        }
+    }
 }
